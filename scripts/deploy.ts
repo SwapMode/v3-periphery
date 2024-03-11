28377b2130df2712d2fce9611b8753f9deb3e5ef
0xshoegazer <0xshoegazer@gmail.com>
2024-03-11 13:38:38 -0400

    factory

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -12,7 +12,7 @@ const hardhat = require('hardhat')
 // ethers.utils.formatBytes32String("ETH")
 const nativeCurrencyLabelBytes = '0x4554480000000000000000000000000000000000000000000000000000000000'
 const WETH = '0x4200000000000000000000000000000000000006'
-const FACTORY = '0xD4f08b675270DFD9FedAeA5E6a879a945BE80d1d'
+const FACTORY = '0x6E36FC34eA123044F278d3a9F3819027B21c9c32'
 const PROTOCOL_TOKEN = '0xFDa619b6d20975be80A10332cD39b9a4b0FAa8BB'
 
 async function main() {
@@ -22,7 +22,7 @@ async function main() {
     // await deployQuoterV2(FACTORY, WETH, PROTOCOL_TOKEN)
     // await deployTickLens()
     // await deployTokenPositionDescriptor(WETH, nativeCurrencyLabelBytes, NFTDescriptorLibrary)
-    // await deployPositionManager(FACTORY, WETH, '0x36d1Ab9DDD6b59e8F2c01F8e3E9D0AdEcA0E864d', PROTOCOL_TOKEN)
+    await deployPositionManager(FACTORY, WETH, '0x36d1Ab9DDD6b59e8F2c01F8e3E9D0AdEcA0E864d', PROTOCOL_TOKEN)
     // await deployMigrator(FACTORY, WETH, NonfungiblePositionManager, signer);
   } catch (error) {
     console.log(error)
